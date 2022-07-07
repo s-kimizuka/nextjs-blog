42317599f34f9c6de72da3aba9bfb4c06ba6e316
s-kimizuka <s-kimizuka@mi-labo.co.jp>
2022-07-07 19:43:04 +0900

    Format Date.

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,6 @@
 import Head from 'next/head';
 import Link from 'next/link';
+import Date from "../components/date";
 import Layout, {siteTitle} from "../components/layout";
 import utilStyles from "../styles/utils.module.css";
 import { getSortedPostsData } from "../lib/posts";
@@ -24,11 +25,13 @@ export default function Home({ allPostData }) {
         <ul className={utilStyles.list}>
           {allPostData.map(( {id, date, title} ) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
-              <br/>
-              {id}
-              <br/>
-              {date}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date}/>
+              </small>
             </li>
           ))}
         </ul>
