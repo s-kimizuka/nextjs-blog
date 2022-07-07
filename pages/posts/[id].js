6867ab32a652488d196ec087ffb87f405da377ba
s-kimizuka <s-kimizuka@mi-labo.co.jp>
2022-07-07 17:50:33 +0900

    Added title to the Post Page.

--- pages/posts/[id].js
+++ pages/posts/[id].js
@@ -1,9 +1,14 @@
+import Head from "next/head";
 import Layout from "../../components/layout";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 
 export default function Post({ postData }) {
   return (
     <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      
       {postData.title}
       <br />
       {postData.id}
