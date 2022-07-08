86f2baf1a04956031520192e7bd33cee2e20f4b2
s-kimizuka <s-kimizuka@mi-labo.co.jp>
2022-07-08 11:02:06 +0900

    Convert javascripts to typescript.

--- pages/posts/[id].tsx
+++ pages/posts/[id].tsx
@@ -3,8 +3,17 @@ import Layout from "../../components/layout";
 import Date from "../../components/date";
 import { getAllPostIds, getPostData } from "../../lib/posts";
 import utilStyles from "../../styles/utils.module.css";
+import { GetStaticProps, GetStaticPaths } from 'next';
 
-export default function Post({ postData }) {
+export default function Post({ 
+    postData
+   } : {
+    postData: {
+      title: string,
+      date: string,
+      contentHtml: string
+    }
+   }) {
   return (
     <Layout>
       <Head>
@@ -22,7 +31,7 @@ export default function Post({ postData }) {
   );
 }
 
-export async function getStaticPaths() {
+export const getStaticPaths: GetStaticPaths = async () => {
   const paths = getAllPostIds();
   return {
     paths,
@@ -30,8 +39,8 @@ export async function getStaticPaths() {
   }
 }
 
-export async function getStaticProps({ params }) {
-  const postData = await getPostData(params.id);
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const postData = await getPostData(params.id as string);
   return {
     props: {
       postData,
